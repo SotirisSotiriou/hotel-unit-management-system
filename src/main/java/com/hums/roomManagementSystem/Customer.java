41cdacc86ebe19a3696aad5b6836b48b6a29ec75
Sotiris Sotiriou <45819898+SotirisSotiriou@users.noreply.github.com>
2020-05-10 15:30:08 +0300

    created customer classes

---
+++ src/main/java/com/hums/roomManagementSystem/Customer.java
@@ -0,0 +1,81 @@
+package com.hums.roomManagementSystem;
+
+import java.time.LocalDate;
+
+public class Customer {
+	private int id;
+	private String lastname;
+	private String firstname;
+	private String phone;
+	private LocalDate firstReservationDate;
+	private RoomReservationList reservationHistory;
+	
+	private static int nextID = 0;
+	
+	public Customer(String lastname, String firstname, String phone, LocalDate date) {
+		this.id = nextID;
+		nextID++;
+		this.lastname = lastname;
+		this.firstname = firstname;
+		this.phone = phone;
+		this.firstReservationDate = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public LocalDate getFirstReservationDate() {
+		return firstReservationDate;
+	}
+
+	public void setFirstReservationDate(LocalDate firstReservationDate) {
+		this.firstReservationDate = firstReservationDate;
+	}
+
+	public static int getNextID() {
+		return nextID;
+	}
+
+	public RoomReservationList getReservationHistory() {
+		return reservationHistory;
+	}
+
+	public void setReservationHistory(RoomReservationList reservationHistory) {
+		this.reservationHistory = reservationHistory;
+	}
+	
+	
+	public String toString() {
+		return "Customer: " + lastname + ", " + firstname + ", " + phone + ", " + "First Reservation: " + firstReservationDate.getDayOfMonth() + "/" + firstReservationDate.getMonthValue() + "/" + firstReservationDate.getYear();
+	}
+	
+}
---
+++ src/main/java/com/hums/roomManagementSystem/CustomerList.java
@@ -0,0 +1,85 @@
+package com.hums.roomManagementSystem;
+
+import java.util.ArrayList;
+
+
+public class CustomerList{
+	private ArrayList<Customer> customers;
+	
+	public CustomerList(ArrayList<Customer> customers) {
+		this.customers = customers;
+	}
+	
+	public CustomerList() {
+		this.customers = new ArrayList<>();
+	}
+
+	public ArrayList<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(ArrayList<Customer> customers) {
+		this.customers = customers;
+	}
+	
+	public void addCustomer(Customer customer) {
+		this.customers.add(customer);
+	}
+	
+	public Customer searchCustomerByName(String lastname, String firstname) {
+		Customer foundCustomer = null;
+		
+		for(Customer customer : this.customers) {
+			if(customer.getLastname().equals(lastname) && customer.getFirstname().equals(firstname)) {
+				foundCustomer = customer;
+				break;
+			}
+		}
+		
+		return foundCustomer;
+	}
+	
+	
+	public Customer searchCustomerByID(int id) {
+		Customer foundCustomer = null;
+		
+		for(Customer customer : this.customers) {
+			if(customer.getId() == id) {
+				foundCustomer = customer;
+				break;
+			}
+		}
+		return foundCustomer;
+	}
+	
+	public void changeCustomerInfo(Customer customer, String lastname, String firstname, String phone) {
+		customer.setLastname(lastname);
+		customer.setFirstname(firstname);
+		customer.setPhone(phone);
+	}
+	
+	
+	public ArrayList<RoomReservation> getReservationsByCustomer(Customer customer, ArrayList<RoomReservation> allReservations) {
+		ArrayList<RoomReservation> reservationsByCustomer = new ArrayList<>();
+		for(RoomReservation rr : allReservations) {
+			if(customer.equals(rr.getCustomer())) {
+				reservationsByCustomer.add(rr);
+			}
+		}
+		return reservationsByCustomer;
+	}
+	
+	public boolean customerExists(String lastname, String firstname, String phone) {
+		boolean exists = false;
+		for(Customer c : this.customers) {
+			if(c.getLastname().equals(lastname) && c.getFirstname().equals(firstname) && c.getPhone().equals(phone)) {
+				exists = true;
+			}
+		}
+		return exists;
+	}
+	
+	public boolean customerExists(Customer customer) {
+		return this.customers.contains(customer);
+	}
+}
