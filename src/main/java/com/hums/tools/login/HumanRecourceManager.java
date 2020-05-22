19d22f5c1d95286e4eba87631723e1f424f13915
sotiris-sotiriou <45819898+sotiris-sotiriou@users.noreply.github.com>
2020-05-22 18:50:39 +0300

    just fixed the name of this class

--- src/main/java/com/hums/tools/login/HumanRecourceManager.java
+++
@@ -1,8 +0,0 @@
-package com.hums.tools.login;
-
-public class HumanRecourceManager extends User{
-
-    public HumanRecourceManager(String username, String password){
-        super(username, password);
-    }
-}
---
+++ src/main/java/com/hums/tools/login/HumanResourceManager.java
@@ -0,0 +1,8 @@
+package com.hums.tools.login;
+
+public class HumanResourceManager extends User{
+
+    public HumanResourceManager(String firstname, String lastname, String username, String password, String email){
+        super(firstname, lastname, username, password, email);
+    }
+}
