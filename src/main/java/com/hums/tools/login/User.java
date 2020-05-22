78a1b65568b0e63c997dbbb3735edebb81091a58
sotiris-sotiriou <45819898+sotiris-sotiriou@users.noreply.github.com>
2020-05-22 18:57:07 +0300

    added getters and setters for some attributes

--- src/main/java/com/hums/tools/login/User.java
+++ src/main/java/com/hums/tools/login/User.java
@@ -30,4 +30,28 @@ public abstract class User{
     public void setPassword(String password){
         this.password = password; 
     }
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 }
