0a2940a508a9baee0346c47a12bb37ba073f86ce
andreaszks <andreaszks20@gmail.com>
2020-05-31 21:52:31 +0300

    Updated cancelReservation method in RoomReservationList

--- src/main/java/com/hums/roomManagementSystem/RoomReservationList.java
+++ src/main/java/com/hums/roomManagementSystem/RoomReservationList.java
@@ -3,8 +3,6 @@ package com.hums.roomManagementSystem;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-import com.hums.Condition;
-
 public class RoomReservationList {
 	private ArrayList<RoomReservation> reservations;
 	
@@ -24,49 +22,15 @@ public class RoomReservationList {
 		this.reservations = reservations;
 	}
 	
-	public ArrayList<RoomReservation> getPresentReservations(){
-		ArrayList<RoomReservation> present = new ArrayList<>();
-		for(RoomReservation r : this.reservations) {
-			if(r.getCondition() == Condition.PRESENT) present.add(r);
-		}
-		return present;
-	}
-	
-	public ArrayList<RoomReservation> getPastReservations(){
-		ArrayList<RoomReservation> past = new ArrayList<>();
-		for(RoomReservation r : this.reservations) {
-			if(r.getCondition() == Condition.PAST) past.add(r);
-		}
-		
-		return past;
-	}
-	
-	public ArrayList<RoomReservation> getFutureReservations() {
-		ArrayList<RoomReservation> future = new ArrayList<>();
-		for(RoomReservation r : this.reservations) {
-			if(r.getCondition() == Condition.FUTURE) future.add(r);
-		}
-		return future;
-	}
 	
 	public void addReservation(RoomReservation reservation) {
 		this.reservations.add(reservation);
 	}
 	
 	public void cancelReservation(RoomReservation reservation) {
-		reservation.setCheckOUT(LocalDate.now());
-		reservation.setEndReason(EndReasonType.CANCELED);
+		reservation.setCheckOutDate(LocalDate.now());
+		reservation.setEndReason(RoomReservation.endReasonType.CANCELED);
 	}
 	
-	public void deleteReservation(RoomReservation reservation) {
-		this.reservations.remove(reservation);
-	}
 	
-	public boolean checkDateAvailability(LocalDate checkIN, LocalDate checkOUT) {
-		for(RoomReservation rr : this.reservations) {
-			if(rr.checkDate(checkIN, checkOUT) == false) return false;
-			
-		}
-		return true;
-	}
 }
