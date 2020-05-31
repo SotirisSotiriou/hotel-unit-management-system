5ed7fc6cbf8a73137115bf36b6fb82a9030e29f1
andreaszks <andreaszks20@gmail.com>
2020-05-31 23:09:09 +0300

    Added 'addReservation' method in EventReservationList

--- src/main/java/com/hums/eventManagementSystem/EventReservationList.java
+++ src/main/java/com/hums/eventManagementSystem/EventReservationList.java
@@ -7,9 +7,38 @@ public class EventReservationList implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	private int nextID=0;
+	
 	private ArrayList<EventReservation> reservations=new ArrayList<EventReservation>();
 	
-	
+	public boolean addReservation(EventReservation e)
+	{
+		
+		if(EMS_Registry.getInstance().getHallList().getHalls().size()!=0) {
+			
+			
+			
+			e.setHall( EMS_Registry.getInstance().getHallList().getAvailableHall(e.getStartTime(), 
+					e.getEndTime(), 
+					e.getInfo().getCapacity()) );
+			
+			nextID++;
+			e.setId(nextID);
+			
+		}else {
+			return false;
+		}
+		
+		
+		
+		if(e.getHall()==null) {
+			return false;
+		}
+		
+		reservations.add(e);
+		return true;
+		
+	}
 	
 	public ArrayList<EventReservation> getReservations() {
 		return reservations;
