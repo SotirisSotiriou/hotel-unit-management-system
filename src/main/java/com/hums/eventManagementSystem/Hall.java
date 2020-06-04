4d50f43a596ca256fff4b0e7ec36aca9d50eef79
Sotiris Sotiriou <sotiris.sot2000@gmail.com>
2020-06-04 17:15:36 +0300

    added serial version id in Hall class

--- src/main/java/com/hums/eventManagementSystem/Hall.java
+++ src/main/java/com/hums/eventManagementSystem/Hall.java
@@ -5,6 +5,10 @@ import java.io.Serializable;
 
 public class Hall implements Comparable<Hall>, Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int capacity;
 	private int code;
 	private String location;
